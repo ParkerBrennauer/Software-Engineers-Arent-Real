c78a1ca578d72e63b5295bfe37a13db2af49553a
Armaan <armaanscheema@gmail.com>
2026-03-05 07:55:13 -0800

    Feat4 - Retrieve All Restaurants
    - created schema, services, pytest, and repo files for restaurant
    - implemented retrieval by search query
    - renamed customer.json to customers.json
    - added "restaurant_id" as an attribute to restaurants.json entries
    - moved Item class from order_schema to restaurant_schema

---
+++ backend/src/schemas/restaurant_schema.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import List
+from src.schemas.order_schema import Order
+
+class Item(BaseModel):
+    restaurantId: int
+    itemName: str
+    cost: float
+    cuisine: str
+    restaurant: str
+    avg_rating: float
+
+class Restaurant(BaseModel):
+    #name: str
+    restaurantId: int
+    menu: List[Item]
+    cuisine: str
+    ratings: dict
+    orders: List[Order]
+
+class RestaurantCreate(BaseModel):
+    #name: str
+    menu: List[Item]
+    cuisine: str
+    ratings: dict
+    orders: List[Order]
+
+class RestaurantUpdate(BaseModel):
+    #name: str
+    menu: List[Item]
+    cuisine: str
+
