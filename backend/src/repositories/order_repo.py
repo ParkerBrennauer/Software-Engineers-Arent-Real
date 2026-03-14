8d4b914f9a3b8dca7813cafaf4b67708f084d068
FakeGuestCA <twuchic123@gmail.com>
2026-03-13 18:28:36 -0700

    Working on order_repo

--- backend/src/repositories/order_repo.py
+++ backend/src/repositories/order_repo.py
@@ -1,17 +1,25 @@
 import os
 import json
-from typing import List
+from src.schemas.order_schema import Order
 import aiofiles
 
 class OrderRepo():
-    FILE_PATH = "backend/src/data/order.json"
+    DATA_PATH = os.path.join(os.path.dirname(__file__), "backend/src/data/order.json")
 
-    async def get_order():
-        pass
-    
+    @staticmethod
     async def get_all_orders():
-        pass
+        
+        with open(OrderRepo.DATA_PATH, "r") as f:
+            return json.load(f)
 
+    @staticmethod
+    async def get_order(order_id: str) -> Order:
+
+        orders = await Order.get_all_orders()
+        if order_id not in orders:
+            raise ValueError("Order not found")
+        return Order(**orders[order_id])      
+    
     async def update_order():
         pass
 
