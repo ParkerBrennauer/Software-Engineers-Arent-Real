3bfb98428098ce7ff4720ef996d49cf715271ec6
FakeGuestCA <twuchic123@gmail.com>
2026-03-17 12:08:42 -0700

    Quick fix for script and fixed orders.json
    As per Jovan's request, I have fixed the order id to numbers (1, 2, 3,...), and I have also added more stuff like order_status, payment_status, delay_reason, and driver.

--- backend/src/scripts/restructure_orders_json.py
+++ backend/src/scripts/restructure_orders_json.py
@@ -5,16 +5,20 @@ with open("backend/src/data/raw/deliveries.json", encoding="utf-8") as f:
 
 fixed_orders = {}
 
-for order in raw:
+for i, order in enumerate(raw, start=1):
 
-    fixed_orders[order["order_id"]] = {
+    fixed_orders[str(i)] = {
         "items": [order["food_item"]],
         "cost": order["order_value"],
         "restaurant": f"Restaurant_{order['restaurant_id']}",
         "customer": order["customer_id"],
         "time": order["delivery_time_actual"],
         "cuisine" : order["preferred_cuisine"],
-        "distance": order["delivery_distance"]
+        "distance": order["delivery_distance"],
+        "order_satus": "payment pending",
+        "payment_status": "pending",
+        "delay_reason": None,
+        "driver": None
     }
 
 with open("backend/src/data/orders.json", "w", encoding="utf-8") as f:
