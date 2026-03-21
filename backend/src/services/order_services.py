4b4c269a28949b52f5f84df56fd9e01f2db02994
FakeGuestCA <twuchic123@gmail.com>
2026-03-21 14:42:37 -0700

    Added more methods for order_service

--- backend/src/services/order_services.py
+++ backend/src/services/order_services.py
@@ -33,3 +33,29 @@ class OrderService:
             delay_reason= reason
         )
         return await OrderRepo.update_order(order_id, update)
+
+    @staticmethod
+    async def get_driver_orders(driver: str) -> list[Order]:
+
+        return await OrderRepo.get_orders_by_driver(driver)
+    
+    @staticmethod
+    async def pickup_order(order_id: str) -> Order:
+
+        update = OrderUpdate(order_status=OrderStatus.PICKED_UP)
+        return await OrderRepo.update_order(order_id, update)
+    
+    @staticmethod
+    async def report_driver_delay(order_id: str, reason: str) -> Order:
+
+        update = OrderUpdate(
+            order_status= OrderStatus.DELAYED,
+            delay_reason= reason
+        )
+        return await OrderRepo.update_order(order_id, update)
+    
+    @staticmethod
+    async def cancel_order(order_id: str) -> Order:
+
+        update = OrderUpdate(order_status=OrderStatus.CANCELLED)
+        return await OrderRepo.update_order(order_id, update)
