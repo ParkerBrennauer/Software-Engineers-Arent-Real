1edc871da9685c92973393a49e61561202d332e9
FakeGuestCA <twuchic123@gmail.com>
2026-03-21 14:54:17 -0700

    Working on order_repo

--- backend/src/repositories/order_repo.py
+++ backend/src/repositories/order_repo.py
@@ -1,3 +1,28 @@
 import os
 import json
 from src.schemas.order_schema import Order, OrderUpdate
+
+class OrderRepo():
+
+    DATA_PATH = os.path.join(os.path.dirname(__file__), "../data/order.json")
+
+    @staticmethod
+    async def get_all_orders():
+
+        with open(OrderRepo.DATA_PATH, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+    @staticmethod
+    async def get_order(order_id: str) -> Order:
+
+        orders = await OrderRepo.get_all_orders()
+        if order_id not in orders:
+            raise ValueError("Order not found")
+        return Order(**orders[order_id])
+    
+    @staticmethod
+    async def update_order(order_id: str, update: OrderUpdate) -> Order:
+
+        orders = await OrderRepo.get_all_orders()
+        if order_id not in orders:
+            raise ValueError("Order not found")
