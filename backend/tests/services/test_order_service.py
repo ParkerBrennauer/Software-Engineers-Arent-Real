19572727841549db789e56a45d96a24bf5f53551
FakeGuestCA <twuchic123@gmail.com>
2026-03-21 15:21:40 -0700

    Added 2 test cases

--- backend/tests/services/test_order_service.py
+++ backend/tests/services/test_order_service.py
@@ -2,3 +2,34 @@ from unittest.mock import AsyncMock, patch
 from src.services.order_services import OrderService
 from src.schemas.order_schema import Order
 import pytest
+
+@pytest.mark.asyncio
+async def test_get_order_status():
+
+    fake_order = Order(
+        items=["Spaghetti"],
+        cost=20.0,
+        restaurant="Trevor's pasta",
+        customer="Joe",
+        time=20,
+        cuisine="Italian",
+        distance=2.5
+    )
+    with patch("src.repositories.order_repo.OrderRepo.get_order",
+               new_callable=AsyncMock) as mock_get:
+        
+        mock_get.return_value = fake_order
+        order = await OrderService.get_order_status("123")
+        assert order == fake_order
+
+@pytest.mark.asyncio
+async def test_mark_ready_for_pickup():
+
+    with patch("src.repositories.order_repo.OrderRepo.update_order",
+                new_callable=AsyncMock) as mock_update:
+        
+        mock_update.return_value = "updated_order"
+        result = await OrderService.mark_ready_for_pickup("123")
+        assert result == "updated_order"
+        mock_update.assert_called_once()
+        assert mock_update.call_args[0][0] == "123"
