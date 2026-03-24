4422b480b495f54dec51b1214ddc14c25118aa2d
Armaan <armaanscheema@gmail.com>
2026-03-24 11:34:04 -0700

    Squashed commit of the following:
    commit a09b2d61e5627b6effd08283eb0c3dd90870b5f1
    Author: Armaan <armaanscheema@gmail.com>
    Date:   Tue Mar 24 11:24:10 2026 -0700
    
        Fixing Merge Conflicts
    
    commit baef4057197b5b063cd853e1583755ed43270e1c
    Author: Armaan <armaanscheema@gmail.com>
    Date:   Thu Mar 19 08:26:55 2026 -0700
    
        Pylint Refactor
    
    commit 13ca975d509587ed35ba629808345c0245642e72
    Author: Armaan <armaanscheema@gmail.com>
    Date:   Thu Mar 19 08:18:16 2026 -0700
    
        Item Setup + Item Retrieval
    
        - removed Item from restaurant schema
        - created item schema
        - create item repo
        - add retrieval by key and restaurant_id methods to repo and services
        - created pytest for all methods
        - pytest passing

--- backend/src/schemas/restaurant_schema.py
+++ backend/src/schemas/restaurant_schema.py
@@ -2,16 +2,6 @@ from pydantic import BaseModel
 from src.schemas.item_schema import ItemBase
 from src.schemas.order_schema import Order
 
-
-class Item(BaseModel):
-    restaurant_id: int
-    item_name: str
-    cost: float
-    cuisine: str
-    restaurant: str
-    avg_rating: float
-
-
 class Restaurant(BaseModel):
     # name: str
     restaurant_id: int
