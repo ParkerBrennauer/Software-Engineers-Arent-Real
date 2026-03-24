fd702ce2b91cbbd3b3138079fd259c95afa3299a
realTat <admin@realtat.com>
2026-03-24 04:32:16 -0700

    Add order tracking service

--- backend/src/services/__init__.py
+++ backend/src/services/__init__.py
@@ -1,4 +1,5 @@
+from src.services.order_tracking_service import OrderTrackingService
 from src.services.restaurant_owner_services import RestaurantOwnerService
 from src.services.user_service import UserService
 
-__all__ = ["UserService", "RestaurantOwnerService"]
+__all__ = ["UserService", "RestaurantOwnerService", "OrderTrackingService"]
