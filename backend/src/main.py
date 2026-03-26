639a08e54cee4666e2d82f7f67acf38382ab1e32
ParkerBrennauer <brennauerparker@gmail.com>
2026-03-25 19:11:58 -0700

    Initialized exception handlers in main.

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,8 +6,11 @@ from src.api.routers.restaurant_administration_router import (
     router as restaurant_administration_router,
 )
 from src.api.routers.user_router import router as user_router
+from src.api.dependencies import setup_exception_handlers
 
 app = FastAPI(title="Software Engineers Aren't Real Backend")
+
+setup_exception_handlers(app)
 
 app.include_router(user_router)
 app.include_router(restaurant_administration_router)
