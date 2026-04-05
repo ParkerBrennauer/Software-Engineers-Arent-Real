c10f0da177022266033ed8255500a88a0c9ca03a
FakeGuestCA <twuchic123@gmail.com>
2026-04-04 17:47:45 -0700

    Update tips_service.py

--- backend/src/services/tips_service.py
+++ backend/src/services/tips_service.py
@@ -6,3 +6,22 @@ class TipService:
             raise ValueError("Tip percent cannot be negative")
 
         return round(subtotal * (tip_percent/100), 2)
+
+    @staticmethod
+    async def apply_tip(order_id: int, tip_percent: float):
+        from src.services.order_services import OrderService
+
+        order = await OrderService.get_order_status(order_id)
+
+        subtotal = order["cost"]
+        tip_amount = TipService.calculate_tip(subtotal, tip_percent)
+
+        updated = await OrderService.update_order(
+            order_id,
+            {
+                "tip_percent": tip_percent,
+                "tip_amount": tip_amount
+            }
+        )
+
+        return updated
